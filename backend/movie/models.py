e33f06e6b7938204cf0c141aa64cf0f562e496b6
hanna <namnam1178@daum.net>
2021-04-27 08:57:51 +0900

    [ADD] Create movie model

--- backend/movie/models.py
+++ backend/movie/models.py
@@ -1,6 +1,21 @@
+from django.conf import settings
 from django.db import models
 
 
 class Movie(models.Model):
-    name = models.CharField(max_length=50)
-    grade = models.CharField(max_length=8)
+    movieCd = models.IntegerField(primary_key=True)
+    movieNm = models.CharField(max_length=20)
+    genreNm = models.CharField(max_length=10)
+    watchGradeNm = models.CharField(max_length=10)
+
+    watched_user = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name='watched_user',
+        through='WatchedUser',
+    )
+
+
+class WatchedUser(models.Model):
+    shop_post = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
