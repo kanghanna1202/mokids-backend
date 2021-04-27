cd838d523726f0b4f990914471b0a93af50b9c71
hanna <namnam1178@daum.net>
2021-04-27 09:11:40 +0900

    [ADD] Create movie serializer

---
+++ backend/movie/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from movie.models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    watched = serializers.SerializerMethodField()
+
+    def get_watched(self, obj):
+        user = self.context.get('request').user
+        if obj.watched_user.filter(id=user.id).exists():
+            return True
+        else:
+            return False
+
+    class Meta:
+        model = Movie
+        fields = ('movieCd', 'movieNm', 'genreNm', 'watchGradeNm', 'watched')
