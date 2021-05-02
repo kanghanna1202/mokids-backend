e28f68f29fa77a4d61098e6d477362c70e5b7327
hanna <namnam1178@daum.net>
2021-05-02 23:27:58 +0900

    [ADD] Create user serializer

---
+++ user/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from user.models import User
+
+
+class UserCreateSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+
+        user.save()
+        return user
